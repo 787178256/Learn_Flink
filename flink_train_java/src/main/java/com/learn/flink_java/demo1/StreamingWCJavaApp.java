d14a15744c5f6d909137180ee2ef01b319870118
kimvra <787178256@qq.com>
2019-05-07 20:36:20 +0800

    refactor

--- flink_train_java/src/main/java/com/learn/flink_java/demo1/StreamingWCJavaApp.java
+++ flink_train_java/src/main/java/com/learn/flink_java/demo1/StreamingWCJavaApp.java
@@ -2,6 +2,7 @@ package com.learn.flink_java.demo1;
 
 import org.apache.flink.api.common.functions.FlatMapFunction;
 import org.apache.flink.api.java.tuple.Tuple2;
+import org.apache.flink.api.java.utils.ParameterTool;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
@@ -12,9 +13,23 @@ import org.apache.flink.util.Collector;
  */
 public class StreamingWCJavaApp {
     public static void main(String[] args) throws Exception {
+
+        int port = 0;
+        String ip = "";
+
+        try {
+            ParameterTool parameterTool = ParameterTool.fromArgs(args);
+            port = parameterTool.getInt("port");
+            ip = parameterTool.get("ip");
+        } catch (Exception e) {
+            System.err.println("获取port, ip异常，设为默认值");
+            port = 9999;
+            e.printStackTrace();
+        }
+
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 
-        DataStreamSource<String> text = env.socketTextStream("localhost", 9999);
+        DataStreamSource<String> text = env.socketTextStream(ip, port);
 
         text.flatMap(new FlatMapFunction<String, Tuple2<String, Integer>>() {
             @Override
